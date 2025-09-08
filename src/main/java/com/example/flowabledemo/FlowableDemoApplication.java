9c58148243f19d20995be081dbc8a669ebaeddc6
baiyingwaming <baiyingwaming@foxmail.com>
2025-09-08 09:09:09 +0800

    202508908

--- src/main/java/com/example/flowabledemo/FlowableDemoApplication.java
+++ src/main/java/com/example/flowabledemo/FlowableDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.flowabledemo.mapper")
+//@MapperScan("com.example.flowabledemo.mapper")
 public class FlowableDemoApplication {
 
     public static void main(String[] args) {
